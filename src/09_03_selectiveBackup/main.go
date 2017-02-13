bbfd74b0b13f5a7afa0530733c3c721abb1b9c67
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-02-13 13:23:22 +0300

    обход по директория и поддиректориям

---
+++ src/09_03_selectiveBackup/main.go
@@ -0,0 +1,73 @@
+package main
+
+/*
+   Выборочное копирование: обход дерева каталогов с целью отбора файлов с заданным расширением,
+   копирование в новую папку.
+*/
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		// вывод справки
+		reference := `
+ИМЯ
+
+selectiveBackup - Копирует файлы из папок с заданным расширением. 
+
+СИНТАКСИС
+
+selectiveBackup КАТАЛОГ  
+
+АВТОР
+
+Виктор Соловьев 
+
+СООБЩЕНИЕ ОБ ОШИБКАХ
+
+Об ошибках сообщайте по адресу <viktor.vladimirovich.solovev@gmail.com>.  
+
+АВТОРСКИЕ ПРАВА
+
+Copyright 2017 Viktor Solovev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+		fmt.Println(reference)
+	} else {
+		// TODO: обход директории
+		readdir(os.Args[1])
+		fmt.Println("Готово")
+	}
+}
+
+func readdir(dir string) {
+	// открываем директорию
+	dh, err := os.Open(dir)
+	if err != nil {
+		return
+	}
+	defer dh.Close()
+	// считывание списка файлов
+	for {
+		fis, err := dh.Readdir(10)
+		if err == io.EOF {
+			break
+		}
+		// вывод списка
+		for _, fi := range fis {
+			fmt.Printf("%s/%s\n", dir, fi.Name())
+			// рекурсивный проход по поддиректориям
+			if fi.IsDir() {
+				readdir(dir + "/" + fi.Name())
+			}
+		}
+	}
+}
