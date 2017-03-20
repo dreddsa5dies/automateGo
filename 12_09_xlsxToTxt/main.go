6a6276c8079274ad2c0c5bc34e8b453200a6b7a0
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-03-20 12:17:11 +0300

    all ok

--- 12_09_xlsxToTxt/main.go
+++ 12_09_xlsxToTxt/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	flags "github.com/jessevdk/go-flags"
+	"github.com/tealeg/xlsx"
 )
 
 var opts struct {
@@ -27,6 +29,42 @@ func main() {
 
 	// запись ошибок и инфы в файл
 	log.SetOutput(fLog)
+
+	excelFileName := pwdDir + "/" + opts.FileOPEN
+
+	log.Printf("Открытие рабочей книги: %v", excelFileName)
+
+	wb, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		log.Fatalf("Ошибка открытия книги %v", err)
+	}
+
+	// Имя страницы
+	i := wb.Sheets[0]
+	log.Printf("Открытие страницы %v", i.Name)
+
+	// генерация файлов
+	for m := 0; m < i.MaxCol; m++ {
+		// создание
+		strName := "saveFile" + strconv.Itoa(m+1) + ".txt"
+		saveFile, err := os.Create(strName)
+		log.Printf("Создание файла отчета %v", saveFile.Name())
+		if err != nil {
+			log.Fatalf("Ошибка создания файла %v", err)
+		}
+		defer saveFile.Close()
+
+		// до максимума строк пройти
+		for u := 0; u < i.MaxRow; u++ {
+			// по столбцу
+			sd := i.Cell(u, m)
+			// и вывести как строку
+			ss, _ := sd.String()
+			// Запись
+			saveFile.WriteString(ss)
+			saveFile.WriteString("\n")
+		}
+	}
 
 	// готово
 	log.Println("Готово")
