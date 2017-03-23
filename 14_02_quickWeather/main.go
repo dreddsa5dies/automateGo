8b41a4ee80137c0d90c5873da45ced03edcd520b
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-03-23 13:02:41 +0300

    так и не разобрал ответ от сервера

--- 14_02_quickWeather/main.go
+++ 14_02_quickWeather/main.go
@@ -16,73 +16,47 @@ var opts struct {
 	Location string `short:"l" long:"location" default:"London" description:"Название населенного пункта"`
 }
 
-// Coordinates struct holds longitude and latitude data in returned
-// JSON or as parameter data for requests using longitude and latitude.
-type Coordinates struct {
-	Longitude float64 `json:"lon"`
-	Latitude  float64 `json:"lat"`
+// разобрать всю структуру ответа JSON
+type coord struct {
+	Lat json.Number `json:"lat"`
+	Lon json.Number `json:"lon"`
 }
 
-// Sys struct contains general information about the request
-// and the surrounding area for where the request was made.
-type Sys struct {
-	Type    int     `json:"type"`
-	ID      int     `json:"id"`
-	Message float64 `json:"message"`
-	Country string  `json:"country"`
-	Sunrise int     `json:"sunrise"`
-	Sunset  int     `json:"sunset"`
+type city struct {
+	ID    json.Number `json:"id"`
+	Name  string      `json:"name"`
+	Coord coord       `json:"coord"`
 }
 
-// Wind struct contains the speed and degree of the wind.
-type Wind struct {
-	Speed float64 `json:"speed"`
-	Deg   float64 `json:"deg"`
+// пока закончил тут
+type weather struct {
+	ID   json.Number `json:"id"`
+	Main string      `json:"main"`
 }
 
-// Weather struct holds high-level, basic info on the returned
-// data.
-type Weather struct {
-	ID          int    `json:"id"`
-	Main        string `json:"main"`
-	Description string `json:"description"`
-	Icon        string `json:"icon"`
+type mainData struct {
+	Temp     json.Number `json:"temp"`
+	TempMin  json.Number `json:"temp_min"`
+	TempMax  json.Number `json:"temp_max"`
+	Pressure json.Number `json:"pressure"`
+	SeaLevel json.Number `json:"sea_level"`
+	Humidity json.Number `json:"humidity"`
+	TempKf   json.Number `json:"temp_kf"`
+	Weather  weather     `json:"weather"`
 }
 
-// Main struct contains the temperates, humidity, pressure for the request.
-type Main struct {
-	Temp      float64 `json:"temp"`
-	TempMin   float64 `json:"temp_min"`
-	TempMax   float64 `json:"temp_max"`
-	Pressure  float64 `json:"pressure"`
-	SeaLevel  float64 `json:"sea_level"`
-	GrndLevel float64 `json:"grnd_level"`
-	Humidity  int     `json:"humidity"`
+type data struct {
+	Dt   json.Number `json:"dt"`
+	Main mainData    `json:"main"`
 }
 
-// Clouds struct holds data regarding cloud cover.
-type Clouds struct {
-	All int `json:"all"`
-}
-
-// CurrentWeatherData above for JSON to be unmarshaled into.
-type CurrentWeatherData struct {
-	GeoPos  Coordinates        `json:"coord"`
-	Sys     Sys                `json:"sys"`
-	Base    string             `json:"base"`
-	Weather []Weather          `json:"weather"`
-	Main    Main               `json:"main"`
-	Wind    Wind               `json:"wind"`
-	Clouds  Clouds             `json:"clouds"`
-	Rain    map[string]float64 `json:"rain"`
-	Snow    map[string]float64 `json:"snow"`
-	Dt      int                `json:"dt"`
-	ID      int                `json:"id"`
-	Name    string             `json:"name"`
-	Cod     string             `json:"cod"`
-	Unit    string
-	Lang    string
-	Key     string
+type weatherData struct {
+	Cod     string      `json:"cod"`
+	Message json.Number `json:"message"`
+	Cnt     json.Number `json:"cnt"`
+	List    []data      `json:"list"`
+	City    city        `json:"city"`
+	Country string      `json:"country"`
 }
 
 func main() {
@@ -113,7 +87,8 @@ func main() {
 	defer resp.Body.Close()
 
 	// создание переменной для хранения ответа
-	var p CurrentWeatherData
+	// необходимо составлять структуру для хранения ответа
+	var p weatherData
 
 	// декодирование ответа API
 	if err = json.NewDecoder(resp.Body).Decode(&p); err != nil {
