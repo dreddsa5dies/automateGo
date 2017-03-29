29a89a836a472ca848844ad72c915bf9c7e9b5e3
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-03-29 10:26:08 +0300

    установка 37 и 38

---
+++ 16_01_testEmail/main.go
@@ -0,0 +1,81 @@
+// smtp + ssl (для Google)
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"net/mail"
+	"net/smtp"
+)
+
+func main() {
+	// от кого кому и что
+	from := mail.Address{"gotestsmtp", "gotestsmtp@gmail.com"}
+	to := mail.Address{"gotest", "gotest@lenta.ru"}
+
+	body := "this is the body line1.\nthis is the body line2.\nthis is the body line3.\n"
+	subject := "Тестовое Golang"
+
+	// удаленный сервер Google (обязательно 587 порт) и данные аутотентификации
+	smtpserver := "smtp.gmail.com:587"
+	auth := smtp.PlainAuth("", "gotestsmtp@gmail.com", "PASSWORD", "smtp.gmail.com")
+
+	// установка заголовка письма
+	header := make(map[string]string)
+	header["From"] = from.String()
+	header["To"] = to.String()
+	header["Subject"] = subject
+
+	// для него тело письма
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + body
+
+	// коннект с SMTP сервером
+	c, err := smtp.Dial(smtpserver)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// без сертификата TLS Gmail не пропустит
+	host, _, _ := net.SplitHostPort(smtpserver)
+	tlc := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         host,
+	}
+	if err = c.StartTLS(tlc); err != nil {
+		log.Panic(err)
+	}
+
+	// аутотентификация
+	if err = c.Auth(auth); err != nil {
+		log.Panic(err)
+	}
+
+	// отправка для КОГО и ЧТО
+	if err = c.Mail(from.Address); err != nil {
+		log.Panic(err)
+	}
+	if err = c.Rcpt(to.Address); err != nil {
+		log.Panic(err)
+	}
+
+	// Само письмо
+	w, err := c.Data()
+	if err != nil {
+		log.Panic(err)
+	}
+	_, err = w.Write([]byte(message))
+	if err != nil {
+		log.Panic(err)
+	}
+	err = w.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	c.Quit()
+}
