a5b1591c10a9d7f8e23b34faf7ac1c44b37b1382
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-03-30 10:54:28 +0300

    прохождение по xlsx

--- 16_03_sendDuesReminders/main.go
+++ 16_03_sendDuesReminders/main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	if len(os.Args) > 1 {
-		println(`Start programm:	./sendDuesReminders`)
+		println(`Запсук программы:	./sendDuesReminders
+		Рассылает сообщения на основании сведений из электронной таблицы об уплате взносов.`)
 	} else {
 		// создание файла log для записи ошибок
 		fLog, err := os.OpenFile(`./.log`, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0640)
@@ -35,8 +36,26 @@ func main() {
 		log.Printf("Открытие страницы: %v", sheet.Name)
 
 		// сколько всего колонок
-		k := sheet.MaxCol
-		log.Printf("Всего колонок: %v", k)
+		lastCol := sheet.MaxCol
+		log.Printf("Всего колонок: %v", lastCol)
+		latestMonth := sheet.Cell(0, lastCol)
+		log.Printf("Крайний месяц: %v", latestMonth.Value)
+
+		// TODO: Проверить состояние уплаты взносов для каждого члена клуба
+		unpaidMembers := make(map[string]string)
+		for r := 2; r <= sheet.MaxRow; r++ {
+			payment := sheet.Cell(r, lastCol).Value
+			if payment != "paid" {
+				name := sheet.Cell(r, 0).Value
+				email := sheet.Cell(r, 1).Value
+				unpaidMembers[name] = email
+				log.Printf("Не заплатил: %v", name)
+			}
+		}
+
+		// TODO: Войти в учетную запись email
+
+		// TODO: Отправить сообщения с напоминанием об уплате
 
 		log.Println("Готово")
 	}
