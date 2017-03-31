f64bafe83f70e4d2ac054e8c2733eb89d51fd960
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-03-31 12:09:01 +0300

    SMS send but Twilio no work in Russia

---
+++ 16_04_twilioTest/main.go
@@ -0,0 +1,44 @@
+// отправка SMS
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/sfreiberg/gotwilio"
+)
+
+var opts struct {
+	SID  string `short:"s" long:"sid" default:"" description:"accountSid"`
+	AUTH string `short:"a" long:"auth" default:"" description:"authToken"`
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		println(`Please ./twilioTest -h`)
+	} else {
+		// разбор флагов
+		flags.Parse(&opts)
+
+		// создание файла log для записи ошибок
+		fLog, err := os.OpenFile(`./.log`, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0640)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer fLog.Close()
+
+		// запись ошибок и инфы в файл и вывод
+		log.SetOutput(io.MultiWriter(fLog, os.Stdout))
+
+		twilio := gotwilio.NewTwilioClient(opts.SID, opts.AUTH)
+
+		from := "+12013807451"
+		to := "+79818500301"
+		message := "Welcome to gotwilio!"
+		twilio.SendSMS(from, to, message, "", "")
+
+		log.Println("Готово")
+	}
+}
