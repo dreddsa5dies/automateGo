68a9b6c85d1681ef918cfb6e73078939edb5a6c4
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-11-01 22:11:46 +0300

    change comment and add helpline

--- 07_01_regexpMail/main.go
+++ 07_01_regexpMail/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 
 	"strings"
@@ -11,25 +12,29 @@ import (
 )
 
 func main() {
-	// создание шаблонов регулярных выражений
-	// email regex
-	regMail, _ := regexp.Compile(`[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}`)
-
-	// чтение из буфера
-	// поиск по шаблонам и добавление в срез
-	text, _ := clipboard.ReadAll()
-	var mailAddr []string
-
-	if regMail.MatchString(text) {
-		mailAddr = regMail.FindAllString(text, -1)
-	}
-
-	// запись среза в буфер обмена
-	if len(mailAddr) > 0 {
-		clipboard.WriteAll(strings.Join(mailAddr, "\n"))
-		fmt.Println("Copied to clipboard:")
-		fmt.Println(strings.Join(mailAddr, "\n"))
+	// helpline
+	if len(os.Args) > 1 && (os.Args[1] == "--help" || os.Args[1] == "-h") {
+		fmt.Printf("Usage: %s\n", os.Args[0])
 	} else {
-		fmt.Println("No email addresses found.")
+		// create email regexp
+		regMail, _ := regexp.Compile(`[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}`)
+
+		// read os buffer
+		// find email regexp
+		text, _ := clipboard.ReadAll()
+		var mailAddr []string
+
+		if regMail.MatchString(text) {
+			mailAddr = regMail.FindAllString(text, -1)
+		}
+
+		// write on os buffer
+		if len(mailAddr) > 0 {
+			clipboard.WriteAll(strings.Join(mailAddr, "\n"))
+			fmt.Println("Copied to clipboard:")
+			fmt.Println(strings.Join(mailAddr, "\n"))
+		} else {
+			fmt.Println("No email addresses found.")
+		}
 	}
 }
